63c56e297c0ca1f967ca29dd3b018be9b5530b3d
Mpho Matli <mphojames9@gmail.com>
2024-09-12 15:23:41 +0200

    html + css pdate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// search-box open close js code
+// search-box open
 let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+let searchBox = document.querySelector(".search-box .bx-search")
 
 searchBox.addEventListener("click", ()=>{
   navbar.classList.toggle("showInput");
@@ -12,7 +11,7 @@ searchBox.addEventListener("click", ()=>{
   }
 });
 
-// sidebar open close js code
+// sidebar open close
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
@@ -24,7 +23,7 @@ navLinks.style.left = "-100%";
 }
 
 
-// sidebar submenu open close js code
+// sidebar submenu
 let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function() {
  navLinks.classList.toggle("show1");
